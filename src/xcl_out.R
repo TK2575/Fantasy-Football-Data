f339ca9b54139b9b7f4d87a42cc3d54af68f5d73
Tom Kain <tkain@patientkeeper.com>
2017-11-21 08:53:39 -0800

    Minor bug fix to excel output

--- src/xcl_out.R
+++ src/xcl_out.R
@@ -9,7 +9,7 @@ write_week_to_xl <- function(week_num) {
   sht_nms <- list('Matches','Team Scoring Summary','Position Scoring Summary','Position Scoring Vs Projection','Roster','Player Scoring Summary','Roster - Expanded')
   map(sht_nms, .f = createSheet, object = wb)
   
-  mtchs <- get_matches_data() %>% filter(Week == week_num) %>% writeWorksheet(wb, ., sheet = 'Matches')
+  mtchs <- get_matches_data() %>% filter(Week == week_num)
   tm_scrng_smry <- get_matches_data() %>% team_summary()
   pstn_scrng_smry <- get_roster_data() %>% team_pos_summary()
   pstn_scrng_vs_prjctn <- get_roster_data() %>% team_pos_summary(mode='vs_proj')
