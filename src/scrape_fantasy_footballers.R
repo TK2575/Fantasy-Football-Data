8af51ed9337a7f2aeb0ae6d83adf69e3beb015a9
Tom <tom.kain@gmail.com>
2019-08-15 22:17:18 -0700

    Some scrape method cleanup and first swing at projection summarization

--- src/scrape_fantasy_footballers.R
+++ src/scrape_fantasy_footballers.R
@@ -5,19 +5,19 @@ library(rvest)
 library(tidyverse)
 library(janitor)
 
-fprof <- makeFirefoxProfile(
-  list(
-    browser.download.folderList = 2L, 
-    browser.download.defaultFolder = here("temp"),
-    browser.download.dir = here("temp"),
-    browser.download.downloadDir = here("temp"),
-    browser.download.lastDir = here("temp"),
-    browser.download.manager.showWhenStarting = FALSE, 
-    browser.helperApps.neverAsk.saveToDisk = "text/csv"
-  )
-)
-
 start_local_server <- function() {
+  fprof <- makeFirefoxProfile(
+    list(
+      browser.download.folderList = 2L, 
+      browser.download.defaultFolder = here("temp"),
+      browser.download.dir = here("temp"),
+      browser.download.downloadDir = here("temp"),
+      browser.download.lastDir = here("temp"),
+      browser.download.manager.showWhenStarting = FALSE, 
+      browser.helperApps.neverAsk.saveToDisk = "text/csv"
+    )
+  )
+  
   rsDriver(
     port = 2525L, 
     browser = "firefox", 
@@ -26,7 +26,9 @@ start_local_server <- function() {
   )
 }
 
-get_fantasy_footballers_projections <- function(rd) {
+get_fantasy_footballers_projections <- function() {
+  rd <- start_local_server()
+  
   url <- "https://www.thefantasyfootballers.com/2019-ultimate-draft-kit/"
   
   client <- rd$client
@@ -44,7 +46,6 @@ get_fantasy_footballers_projections <- function(rd) {
   Sys.sleep(10)
   
   # get each position for each expert
-  
   experts <- c("andy","jason","mike")
   positions <- c("QB","RB","WR","TE")
   
@@ -81,5 +82,14 @@ get_fantasy_footballers_projections <- function(rd) {
   }
   client$close()
   #TODO name cleanup (split name, team, bye), drop points
-  result
+  result[is.na(result)] <- 0
+  
+  result %>% 
+    select(-points) %>% 
+    rename(pass_yards = yards,
+           rush_yards = ru_yards,
+           rush_att = ru_att,
+           pass_tds = t_ds,
+           rush_tds = ru_t_ds,
+           rec_tds = rec_t_ds)
 }
