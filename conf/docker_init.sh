abfc7db32a405bbd60889f26c2f2495b608a1cb5
Tom <tom.kain@gmail.com>
2021-01-08 09:11:46 -0500

    Add rstudio server to docker

--- conf/docker_init.sh
+++ conf/docker_init.sh
@@ -9,3 +9,10 @@ docker run -d \
 -p 4445:4444 -p 5901:5900 \
 --memory 4096mb --shm-size 4g \
 selenium/standalone-firefox-debug:latest
+
+docker run -d \
+-v ~/repos/rFF_legacy:/rFF_legacy \
+-p 8787:8787 \
+-e ROOT=TRUE \
+-e PASSWORD=somePassword \
+rocker/tidyverse
